c9b9cea4eb7d568e6c1644b6c897b5cf4c25f539
Sam Wellander <samwellander@gmail.com>
2018-08-14 19:11:59 -0400

    start webcam

--- src/App.js
+++ src/App.js
@@ -22,6 +22,36 @@ class App extends Component {
     web3.eth.getAccounts()
       .then(response => this.setState({ account: response[0] }))
       .catch(err => console.log(err))
+
+    initWebCam();
+  }
+
+  takeSnapshot() {
+
+      var hidden_canvas = document.querySelector('canvas'),
+          video = document.querySelector('video.camera_stream'),
+          image = document.querySelector('img.photo'),
+
+          // Get the exact size of the video element.
+          width = video.videoWidth,
+          height = video.videoHeight,
+
+          // Context object for working with the canvas.
+          context = hidden_canvas.getContext('2d');
+
+      // Set the canvas to the same dimensions as the video.
+      hidden_canvas.width = width;
+      hidden_canvas.height = height;
+
+      // Draw a copy of the current frame from the video on the canvas.
+      context.drawImage(video, 0, 0, width, height);
+
+      // Get an image dataURL from the canvas.
+      var imageDataURL = hidden_canvas.toDataURL('image/png');
+
+      // Set the dataURL as source of an image element, showing the captured photo.
+      image.setAttribute('src', imageDataURL); 
+
   }
 
   render() {
@@ -37,9 +67,43 @@ class App extends Component {
           Balance: { this.state.balance } AC
         </p>
 
+        <video id='video'>
+        </video>
+
+        <button onClick={() => this.takeSnapShot()}>Snap</button>
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+const initWebCam = () => {
+  navigator.getUserMedia(
+      // Options
+      {
+          video: true
+      },
+      // Success Callback
+      function(stream){
+          const video = document.getElementById('video');
+          // Create an object URL for the video stream and
+          // set it as src of our HTLM video element.
+          video.src = window.URL.createObjectURL(stream);
+
+          // Play the video element to show the stream to the user.
+          video.play();
+
+      },
+      // Error Callback
+      function(err){
+
+          // Most common errors are PermissionDenied and DevicesNotFound.
+          console.error(err);
+
+      }
+  );
+}
+
