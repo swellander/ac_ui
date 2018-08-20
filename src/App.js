bbbe0cbdb4b6e6fc9cdd7c31950369baacae5f48
Sam Wellander <samwellander@gmail.com>
2018-08-20 09:19:15 -0400

    add request AC

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import ac from './attendanceCoin';
 import web3 from './web3';
-import { initWebCam } from './util.js';
-import Button from 'antd/lib/button';
+import { takeSnapshot, initWebCam } from './util.js';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
@@ -14,10 +14,14 @@ class App extends Component {
     this.state = {
       account: '',
       balance: 0,
-      toAddress: ''
+      toAddress: '',
+      latitude: 0,
+      longitude: 0
     }
+
     this.handleChange = this.handleChange.bind(this);
     this.sendCoin = this.sendCoin.bind(this);
+    this.requestAC = this.requestAC.bind(this);
   }
   
   sendCoin() {
@@ -30,63 +34,47 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    
+    //listen for events
+    // transferEvent.watch((err, event) => {
+    //   console.log('err', err);
+    //   console.log('event', event);
+    // });
+
     const [account] = await web3.eth.getAccounts()
     this.setState({ account })     
-
     const balance = await ac.methods.balanceOf(this.state.account).call();
     this.setState({ balance })
-      // .then(balance => {
-      //   console.log('hey')
-      //   this.setState({ balance })
-      // })
-      // .catch(err => console.log('ERROR', err))
-
-    
-
   }
 
   startWebCam() {
     initWebCam();
-    console.log('webCam init')
   }
 
   takeSnapshot() {
-
-      var hidden_canvas = document.querySelector('canvas'),
-          video = document.querySelector('video'),
-          image = document.querySelector('#snapshot'),
-
-          // Get the exact size of the video element.
-          width = video.videoWidth,
-          height = video.videoHeight,
-
-          // Context object for working with the canvas.
-          context = hidden_canvas.getContext('2d');
-
-      // Set the canvas to the same dimensions as the video.
-      hidden_canvas.width = width;
-      hidden_canvas.height = height;
-
-      // Draw a copy of the current frame from the video on the canvas.
-      context.drawImage(video, 0, 0, width, height);
-
-      // Get an image dataURL from the canvas.
-      var imageDataURL = hidden_canvas.toDataURL('image/png');
-
-      //clear canvas
-      context.clearRect(0,0, width, height);
-
-      // Set the dataURL as source of an image element, showing the captured photo.
-      image.setAttribute('src', imageDataURL); 
-
+    takeSnapshot(); 
   }
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value })
   }
 
-  
+  requestAC() {
+    console.log('Processing your request...');
+
+    //get geolocation, promps user to allow program to access location
+    navigator.geolocation.getCurrentPosition(position => {
+
+      //gather and send account and location to admin server
+      const data = { 
+        account: this.state.account, 
+        longitude: position.coords.longitude,
+        latitude: position.coords.latitude
+      };
+      axios.post('http://localhost:3002/', data)
+        .then( response => console.log(response.data) )
+        .catch( err => console.log(err));
+    })
+  }
 
   render() {
     const address = this.state.account ? this.state.account : `Please log into MetaMask and refresh`;
@@ -100,16 +88,16 @@ class App extends Component {
         <p className="App-intro">
           Balance: { balance } AC
         </p>
-        
-
+        <button onClick={this.requestAC}>Request AC</button> 
+        <hr/>
         <video>
         </video>
         <canvas></canvas>
         <img id="snapshot" />
 
 
-        <Button onClick={() => this.takeSnapshot()}>Snap</Button>
-        <Button onClick={() => this.startWebCam()}>Start Cam</Button>
+        <button onClick={() => this.takeSnapshot()}>Snap</button>
+        <button onClick={() => this.startWebCam()}>Start Cam</button>
 
         <br/>
         <hr/>
@@ -125,7 +113,7 @@ class App extends Component {
         <input value={this.state.sendAddress} onChange={this.handleChange} type="text" name="amount" placeholder="30" />
         <br/>
         <br/>
-        <Button onClick={this.sendCoin}>Send</Button>
+        <button onClick={this.sendCoin}>Send</button>
       </div>
     );
   }
--- src/util.js
+++ src/util.js
@@ -1,3 +1,32 @@
+export const takeSnapshot = () => {
+  var hidden_canvas = document.querySelector('canvas'),
+          video = document.querySelector('video'),
+          image = document.querySelector('#snapshot'),
+
+          // Get the exact size of the video element.
+          width = video.videoWidth,
+          height = video.videoHeight,
+
+          // Context object for working with the canvas.
+          context = hidden_canvas.getContext('2d');
+
+      // Set the canvas to the same dimensions as the video.
+      hidden_canvas.width = width;
+      hidden_canvas.height = height;
+
+      // Draw a copy of the current frame from the video on the canvas.
+      context.drawImage(video, 0, 0, width, height);
+
+      // Get an image dataURL from the canvas.
+      var imageDataURL = hidden_canvas.toDataURL('image/png');
+
+      //clear canvas
+      context.clearRect(0,0, width, height);
+
+      // Set the dataURL as source of an image element, showing the captured photo.
+      image.setAttribute('src', imageDataURL);
+}
+
 export const initWebCam = () => {
   navigator.getUserMedia(
       // Options
